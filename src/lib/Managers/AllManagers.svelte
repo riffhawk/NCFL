fd5b734ecda3379ff388e76b0922a18ab3ad21c5
riffhawk <69264575+riffhawk@users.noreply.github.com>
2023-08-12 10:28:53 +0000

    yes

--- src/lib/Managers/AllManagers.svelte
+++ src/lib/Managers/AllManagers.svelte
@@ -1,6 +1,6 @@
 <script>
     import { leagueName } from '$lib/utils/helper';
-    import { fade } from 'svelte/transition'; 
+    import { fade } from 'svelte/transition';
     import ManagerRow from './ManagerRow.svelte';
 
     export let managers, leagueTeamManagers;
@@ -28,76 +28,73 @@
 <svelte:window bind:innerWidth={innerWidth} />
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
-    @import 'skeleton-css/skeleton.css'; /* Import Skeleton styles */
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
 
-    h2 {
-        font-family: 'Poppins', sans-serif;
-        color: #333;
-    }
+
 
     .managerContainer {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin: 4em 0;
+        max-width: 900px;
+        margin: 2em auto;
         padding: 2em;
-        background-color: #f9f9f9;
-        border-radius: 10px;
-        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
-    }
-
-    .managerConstrained {
-        width: 97%;
-        max-width: 800px;
-        padding: 2em;
-        background-color: #fff;
-        border-radius: 10px;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+        background-color: #ffffff;
+        border-radius: 12px;
+        box-shadow: 0 6px 15px rgba(0, 0, 0, 0.1);
     }
 
     h2 {
         text-align: center;
-        font-size: 2.4em;
-        margin-bottom: 2em;
-        letter-spacing: -0.8px;
+        font-weight: 700;
+        font-size: 2rem;
+        margin-bottom: 1.5em;
+        color: #007BFF;
     }
 
     @media (max-width: 520px) {
         h2 {
-            font-size: 1.8em;
-            margin: 1.5em 0 1em;
+            font-size: 1.6rem;
+            margin-bottom: 1em;
         }
     }
 
     .carouselBtn {
         background-color: #007BFF;
         border: none;
-        padding: 12px 24px;
+        padding: 10px 20px;
         margin: 10px;
         cursor: pointer;
         color: #ffffff;
-        border-radius: 5px;
-        transition: background-color 0.3s ease, transform 0.2s ease;
+        border-radius: 8px;
+        font-weight: 500;
+        transition: background-color 0.3s, transform 0.2s;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     }
 
-    .carouselBtn:hover {
+    .carouselBtn:hover, .carouselBtn:focus {
         background-color: #0056b3;
         transform: scale(1.05);
+    }
+    
+    .btnContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        margin-top: 20px;
     }
 </style>
 
 <div class="managerContainer">
     <h2>{leagueName} Managers</h2>
-    <div class="managerConstrained">
-        <div in:fade={{ duration: 500 }}>
-            <ManagerRow 
-                manager={managers[currentManagerIndex]} 
-                leagueTeamManagers={leagueTeamManagers} 
-                key={currentManagerIndex} />
-        </div>
-        
-        <button class="carouselBtn" on:click={previousManager}>Previous</button>
-        <button class="carouselBtn" on:click={nextManager}>Next</button>
+    
+    <div in:fade={{ duration: 300 }}>
+        <ManagerRow 
+            manager={managers[currentManagerIndex]} 
+            leagueTeamManagers={leagueTeamManagers} 
+            key={currentManagerIndex} />
+    </div>
+
+    <div class="btnContainer">
+        <button class="carouselBtn" on:click={previousManager}>← Previous</button>
+        <button class="carouselBtn" on:click={nextManager}>Next →</button>
     </div>
 </div>
