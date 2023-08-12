2db3c4396d6ef74802d72708f62075520b69566a
riffhawk <69264575+riffhawk@users.noreply.github.com>
2023-08-12 00:43:46 +0000

    Lol

--- src/lib/Managers/AllManagers.svelte
+++ src/lib/Managers/AllManagers.svelte
@@ -1,6 +1,6 @@
 <script>
     import { leagueName } from '$lib/utils/helper';
-    import ManagerRow from './ManagerRow.svelte'
+    import ManagerRow from './ManagerRow.svelte';
 
     export let managers, leagueTeamManagers;
 
@@ -10,31 +10,59 @@
 <svelte:window bind:innerWidth={innerWidth} />
 
 <style>
+    /* Using Roboto as an example; ensure it's available or replace with another font */
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    body, h2 {
+        font-family: 'Roboto', sans-serif;
+    }
+
     .managerContainer {
         width: 100%;
-        margin: 2em 0 5em;
+        margin: 4em 0;
+        background-color: #f9f9f9; /* Light grayish background for a subtle difference */
+        padding: 2em;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); /* Subtle shadow */
     }
 
     .managerConstrained {
         width: 97%;
         max-width: 800px;
         margin: 0 auto;
+        padding: 2em;
+        background-color: #fff;
+        border-radius: 8px;
     }
 
     h2 {
         text-align: center;
         font-size: 2.8em;
-        margin: 2em 0 1.5em;
-        line-height: 1em;
+        margin-bottom: 2em;
+        color: #333; /* Darker text for better readability */
     }
 
     @media (max-width: 520px) {
         h2 {
-            text-align: center;
             font-size: 2em;
             margin: 1.5em 0 1em;
-            line-height: 1em;
         }
+    }
+
+    /* Enhancing the buttons */
+    .carouselBtn {
+        background-color: #007BFF; /* A vibrant blue */
+        border: none;
+        padding: 10px 20px;
+        margin: 10px;
+        cursor: pointer;
+        color: white; /* Text color */
+        border-radius: 4px;
+        transition: background-color 0.3s ease; /* Transition effect */
+    }
+
+    .carouselBtn:hover {
+        background-color: #0056b3; /* A darker blue on hover */
     }
 </style>
 
@@ -45,5 +73,4 @@
             <ManagerRow {manager} {leagueTeamManagers} {key} />
         {/each}
     </div>
-
 </div>
