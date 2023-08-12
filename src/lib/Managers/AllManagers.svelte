582ca2af30bb3a3d0a3e5bd5bd7ee9bbf0164b47
riffhawk <69264575+riffhawk@users.noreply.github.com>
2023-08-12 10:26:04 +0000

    Lol

--- src/lib/Managers/AllManagers.svelte
+++ src/lib/Managers/AllManagers.svelte
@@ -1,18 +1,18 @@
 <script>
     import { leagueName } from '$lib/utils/helper';
-    import { fade } from 'svelte/transition'; // Import the fade transition
+    import { fade } from 'svelte/transition'; 
     import ManagerRow from './ManagerRow.svelte';
 
-    let managers, leagueTeamManagers;
+    export let managers, leagueTeamManagers;
 
     let innerWidth;
-    let currentManagerIndex = 0;  // State variable for carousel position
+    let currentManagerIndex = 0;
 
     function nextManager() {
         if (currentManagerIndex < managers.length - 1) {
             currentManagerIndex++;
         } else {
-            currentManagerIndex = 0;  // Loop back to the first item
+            currentManagerIndex = 0; 
         }
     }
 
@@ -20,7 +20,7 @@
         if (currentManagerIndex > 0) {
             currentManagerIndex--;
         } else {
-            currentManagerIndex = managers.length - 1;  // Loop back to the last item
+            currentManagerIndex = managers.length - 1; 
         }
     }
 </script>
@@ -28,55 +28,39 @@
 <svelte:window bind:innerWidth={innerWidth} />
 
 <style>
-    /* Utilizing modern fonts for a sleek appearance */
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+    @import 'skeleton-css/skeleton.css'; /* Import Skeleton styles */
 
     h2 {
         font-family: 'Poppins', sans-serif;
-        color: #333; /* Soft black for text */
+        color: #333;
     }
 
     .managerContainer {
-        /* ... existing styles ... */
         display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
+        flex-direction: column;
+        align-items: center;
+        margin: 4em 0;
+        padding: 2em;
+        background-color: #f9f9f9;
+        border-radius: 10px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
     }
 
-    .managerItem {
-        flex: 1 1 calc(33.333% - 20px); /* Taking up roughly a third, minus margins for space */
-        margin: 10px;
-        padding: 20px;
+    .managerConstrained {
+        width: 97%;
+        max-width: 800px;
+        padding: 2em;
         background-color: #fff;
         border-radius: 10px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-        text-align: center;
     }
 
-    img {
-        max-width: 100%; /* Ensuring images don't break layout */
-        border-radius: 5px; /* Optional, for some styling */
-        margin-bottom: 15px;
-    }
-
-    h3 {
-        margin-bottom: 10px;
-        color: #007BFF; /* Giving color to manager name for some contrast */
-    }
-
-    /* Adjusting for smaller screens */
-    @media (max-width: 520px) {
-        .managerItem {
-            flex: 1 1 100%; /* On smaller screens, manager item will take full width */
-        }
-    }
-
-    /* Titles styles */
     h2 {
         text-align: center;
         font-size: 2.4em;
         margin-bottom: 2em;
-        letter-spacing: -0.8px; /* Tighten the letter spacing for modern feel */
+        letter-spacing: -0.8px;
     }
 
     @media (max-width: 520px) {
@@ -86,69 +70,34 @@
         }
     }
 
-    /* Button styling with hover transition */
     .carouselBtn {
-        background-color: #007BFF; /* Vibrant blue for a modern touch */
+        background-color: #007BFF;
         border: none;
         padding: 12px 24px;
-        margin-top: 10px;
+        margin: 10px;
         cursor: pointer;
-        color: #ffffff; /* White text for contrast */
+        color: #ffffff;
         border-radius: 5px;
-        transition: background-color 0.3s ease, transform 0.2s ease; /* Smooth transitions */
+        transition: background-color 0.3s ease, transform 0.2s ease;
     }
 
     .carouselBtn:hover {
-        background-color: #0056b3; /* Darken on hover */
-        transform: scale(1.05); /* Slightly increase size for a dynamic feel */
+        background-color: #0056b3;
+        transform: scale(1.05);
     }
 </style>
 
-
-
 <div class="managerContainer">
-    <h2>RIFF</h2>
-
-    <!-- Manager Row Item -->
-    <div class="managerItem">
-        <!-- Manager content goes here. For instance: -->
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src="managerImage.jpg" alt="Manager Image" />
-        <h3>Manager Name</h3>
-        <p>Some details about the manager.</p>
-        <button class="carouselBtn">View More</button>
+    <h2>{leagueName} Managers</h2>
+    <div class="managerConstrained">
+        <div in:fade={{ duration: 500 }}>
+            <ManagerRow 
+                manager={managers[currentManagerIndex]} 
+                leagueTeamManagers={leagueTeamManagers} 
+                key={currentManagerIndex} />
+        </div>
+        
+        <button class="carouselBtn" on:click={previousManager}>Previous</button>
+        <button class="carouselBtn" on:click={nextManager}>Next</button>
     </div>
-</div>
-
-<div class="managerContainer">
-    <h2>MIGZ</h2>
-
-    <!-- Manager Row Item -->
-    <div class="managerItem">
-        <!-- Manager content goes here. For instance: -->
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src="managerImage.jpg" alt="Manager Image" />
-        <h3>Manager Name</h3>
-        <p>Some details about the manager.</p>
-        <button class="carouselBtn">View More</button>
-    </div>
-
-    <!-- Repeat the above "managerItem" div for each manager -->
-</div>
-
-
-<div class="managerContainer">
-    <h2>ANGEL</h2>
-
-    <!-- Manager Row Item -->
-    <div class="managerItem">
-        <!-- Manager content goes here. For instance: -->
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src="managerImage.jpg" alt="Manager Image" />
-        <h3>Manager Name</h3>
-        <p>Some details about the manager.</p>
-        <button class="carouselBtn">View More</button>
-    </div>
-
-    <!-- Repeat the above "managerItem" div for each manager -->
 </div>
