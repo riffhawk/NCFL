a73fc83f3b358a9e71f2d313935b41afc1e0f7d1
riffhawk <69264575+riffhawk@users.noreply.github.com>
2023-08-12 01:13:39 +0000

    changes

--- src/lib/Managers/AllManagers.svelte
+++ src/lib/Managers/AllManagers.svelte
@@ -3,7 +3,7 @@
     import { fade } from 'svelte/transition'; // Import the fade transition
     import ManagerRow from './ManagerRow.svelte';
 
-    export let managers, leagueTeamManagers;
+    let managers, leagueTeamManagers;
 
     let innerWidth;
     let currentManagerIndex = 0;  // State variable for carousel position
@@ -31,31 +31,44 @@
     /* Utilizing modern fonts for a sleek appearance */
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
 
-    /* Base styles for better default looks */
     h2 {
         font-family: 'Poppins', sans-serif;
         color: #333; /* Soft black for text */
     }
 
-    /* Container for the managers with added padding and shadow for depth */
     .managerContainer {
-        width: 100%;
-        margin: 4em 0;
-        padding: 2em;
-        background-color: #f9f9f9; /* Soft light gray for subtle depth */
-        border-radius: 10px;
-        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05); /* Gentle shadow */
+        /* ... existing styles ... */
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
     }
 
-    /* Centralized content with a max-width for better reading */
-    .managerConstrained {
-        width: 97%;
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 2em;
-        background-color: #fff; /* Pure white for contrast */
+    .managerItem {
+        flex: 1 1 calc(33.333% - 20px); /* Taking up roughly a third, minus margins for space */
+        margin: 10px;
+        padding: 20px;
+        background-color: #fff;
         border-radius: 10px;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1); /* Light shadow for depth */
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+        text-align: center;
+    }
+
+    img {
+        max-width: 100%; /* Ensuring images don't break layout */
+        border-radius: 5px; /* Optional, for some styling */
+        margin-bottom: 15px;
+    }
+
+    h3 {
+        margin-bottom: 10px;
+        color: #007BFF; /* Giving color to manager name for some contrast */
+    }
+
+    /* Adjusting for smaller screens */
+    @media (max-width: 520px) {
+        .managerItem {
+            flex: 1 1 100%; /* On smaller screens, manager item will take full width */
+        }
     }
 
     /* Titles styles */
@@ -78,7 +91,7 @@
         background-color: #007BFF; /* Vibrant blue for a modern touch */
         border: none;
         padding: 12px 24px;
-        margin: 10px;
+        margin-top: 10px;
         cursor: pointer;
         color: #ffffff; /* White text for contrast */
         border-radius: 5px;
@@ -92,18 +105,50 @@
 </style>
 
 
+
 <div class="managerContainer">
-    <h2>{leagueName} Managers</h2>
-    <div class="managerConstrained">
-        <!-- Wrap ManagerRow in a div and apply the fade transition to that div -->
-        <div in:fade={{ duration: 500 }}>
-            <ManagerRow 
-                manager={managers[currentManagerIndex]} 
-                leagueTeamManagers={leagueTeamManagers} 
-                key={currentManagerIndex} />
-        </div>
-        
-        <button class="carouselBtn" on:click={previousManager}>Previous</button>
-        <button class="carouselBtn" on:click={nextManager}>Next</button>
+    <h2>RIFF</h2>
+
+    <!-- Manager Row Item -->
+    <div class="managerItem">
+        <!-- Manager content goes here. For instance: -->
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="managerImage.jpg" alt="Manager Image" />
+        <h3>Manager Name</h3>
+        <p>Some details about the manager.</p>
+        <button class="carouselBtn">View More</button>
     </div>
+</div>
+
+<div class="managerContainer">
+    <h2>MIGZ</h2>
+
+    <!-- Manager Row Item -->
+    <div class="managerItem">
+        <!-- Manager content goes here. For instance: -->
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="managerImage.jpg" alt="Manager Image" />
+        <h3>Manager Name</h3>
+        <p>Some details about the manager.</p>
+        <button class="carouselBtn">View More</button>
+    </div>
+
+    <!-- Repeat the above "managerItem" div for each manager -->
+</div>
+
+
+<div class="managerContainer">
+    <h2>ANGEL</h2>
+
+    <!-- Manager Row Item -->
+    <div class="managerItem">
+        <!-- Manager content goes here. For instance: -->
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="managerImage.jpg" alt="Manager Image" />
+        <h3>Manager Name</h3>
+        <p>Some details about the manager.</p>
+        <button class="carouselBtn">View More</button>
+    </div>
+
+    <!-- Repeat the above "managerItem" div for each manager -->
 </div>
